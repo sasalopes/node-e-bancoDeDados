bf049ea75d4db75b6f5fc26c39492edf3d7f9cd1
Sabrina Lopes <sasalopesoliveira@gmail.com>
2023-04-17 17:13:59 -0300

    conectando node ao banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-// Vai disponibilizar o uso de variáveis de ambiente
+// Importações principais e variáveis de ambiente
 require("dotenv").config();
+const express = require("express");
 
+//Configuração do App
+const app = express();
+app.use (express.json()); // Possibilitar transitar dados usando JSON
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+//Configuração do Banco de Dados
+const {connection, authenticate} = require("./database/database");
+authenticate(connection); // efetivar a conexão
+//Configurar o model da aplicação
+const Cliente = require("./database/cliente");
+const Endereco = require("./database/Endereco");
+
+// Definição de rotas
+app.get("/clientes", async (req,res) => {
+    //SELECT * FROM clientes;
+    const listaClientes = await Cliente.findAll()
+    res.json(listaClientes);
+});
+
+app.post("/clientes", async (req, res) => {
+    // Coletar os dados do req.body
+    const { nome, email, telefone, endereco } = req.body;
+  
+    try {
+      // Dentro de 'novo' estará o o objeto criado
+      const novo = await Cliente.create(
+        { nome, email, telefone, endereco }, //Permite inserir cliente e endereço num comando
+        { include: [Endereco]}
+        );
+      res.status(201).json(novo);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Um erro aconteceu." });
+    }
+  });
+
+//Escuta de eventos(listen)
+app.listen(5000, () => {
+    //gerar as tabelas a partir do model
+    //Force = apaga tudo e recria as tabelas
+    connection.sync({force:true});
+console.log("Servidor rodando em http://localhost:3000");
+})
